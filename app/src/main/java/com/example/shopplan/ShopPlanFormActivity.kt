401557c7869b8dd7e9f183a0160a09376b4ca7b9
James Kubiak <kubiak.jakub01@gmail.com>
2023-05-29 22:06:33 +0200

    Init ShopPlanFormActivity

---
+++ app/src/main/java/com/example/shopplan/ShopPlanFormActivity.kt
@@ -0,0 +1,12 @@
+package com.example.shopplan
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ShopPlanFormActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_plan_form)
+    }
+}
