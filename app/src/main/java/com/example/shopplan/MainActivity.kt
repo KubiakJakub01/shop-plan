a8d7aa505a43c18cba9a5a61e3609b6ce0eeb7b0
James Kubiak <kubiak.jakub01@gmail.com>
2023-05-29 22:05:58 +0200

    Add openShopPlanForm function

--- app/src/main/java/com/example/shopplan/MainActivity.kt
+++ app/src/main/java/com/example/shopplan/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.shopplan
 
 import android.os.Bundle
+import android.content.Intent
+import android.view.View
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,37 +13,30 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.recyclerview.widget.RecyclerView
 import com.example.shopplan.ui.theme.ShopPlanTheme
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var rvShopPlans: RecyclerView
+//    private lateinit var shopPlanAdapter: ShopPlanAdapter
+    private lateinit var btnCreateShopPlan: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            ShopPlanTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
+        setContentView(R.layout.activity_main)
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+        rvShopPlans = findViewById(R.id.rvShopPlans)
+        btnCreateShopPlan = findViewById(R.id.btnCreateShopPlan)
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ShopPlanTheme {
-        Greeting("Android")
+        rvShopPlans.adapter = ShopPlanAdapter(this)
+        rvShopPlans.setHasFixedSize(true)
     }
+
+    fun openShopPlanForm(view: View) {
+        val intent = Intent(this, ShopPlanFormActivity::class.java)
+        startActivity(intent)
+    }
+
+
 }
