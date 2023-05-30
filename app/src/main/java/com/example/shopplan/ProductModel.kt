67c79a6b33da1343b659183956e3a4f56102febc
James Kubiak <kubiak.jakub01@gmail.com>
2023-05-30 23:17:48 +0200

    Implement Parcelable in ProductModel

--- app/src/main/java/com/example/shopplan/ProductModel.kt
+++ app/src/main/java/com/example/shopplan/ProductModel.kt
@@ -1,5 +1,36 @@
 package com.example.shopplan
 
+import android.os.Parcel
+import android.os.Parcelable
+import java.io.Serializable
+
 data class ProductModel(
     val name: String, val price: Double, var quantity: Int
-)
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString().toString(),
+        parcel.readDouble(),
+        parcel.readInt()
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(name)
+        parcel.writeDouble(price)
+        parcel.writeInt(quantity)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<ProductModel> {
+        override fun createFromParcel(parcel: Parcel): ProductModel {
+            return ProductModel(parcel)
+        }
+
+        override fun newArray(size: Int): Array<ProductModel?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
