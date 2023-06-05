b461f1e32b1c9e1fd69da3cc9cb4396c6a3e0050
James Kubiak <kubiak.jakub01@gmail.com>
2023-06-05 22:49:11 +0200

    Handle with onActivityResult

--- app/src/main/java/com/example/shopplan/MainActivity.kt
+++ app/src/main/java/com/example/shopplan/MainActivity.kt
@@ -1,42 +1,61 @@
 package com.example.shopplan
 
+import android.app.Activity
 import android.os.Bundle
 import android.content.Intent
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.shopplan.ui.theme.ShopPlanTheme
 
 class MainActivity : ComponentActivity() {
 
+    private val TAG = "MainActivity"
+    private lateinit var adapter: ShopPlanAdapter
     private lateinit var rvShopPlans: RecyclerView
 //    private lateinit var shopPlanAdapter: ShopPlanAdapter
     private lateinit var btnCreateShopPlan: Button
-
+    companion object {
+        private const val SHOP_PLAN_FORM_REQUEST_CODE = 1
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        adapter = ShopPlanAdapter()
         rvShopPlans = findViewById(R.id.rvShopPlans)
-        btnCreateShopPlan = findViewById(R.id.btnCreateShopPlan)
-
-        rvShopPlans.adapter = ShopPlanAdapter(this)
+        rvShopPlans.adapter = adapter
+        rvShopPlans.layoutManager = LinearLayoutManager(this)
         rvShopPlans.setHasFixedSize(true)
+
+        btnCreateShopPlan = findViewById(R.id.btnCreateShopPlan)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        Log.i(TAG, "onActivityResult")
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Check if the request code is the same as what is passed here
+        if (requestCode == SHOP_PLAN_FORM_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            // Retrieve the information from the result intent
+            val title = data?.getStringExtra("title")
+            val shop = data?.getStringExtra("shopName")
+            val totalCost = data?.getDoubleExtra("totalCost", 0.0)
+            val products = data?.getParcelableArrayListExtra<ProductModel>("products")
+
+            // Do something with the retrieved information
+            val shopPlan = ShopPlanModel(title.toString(), shop.toString(),
+                totalCost.toString().toDouble(), products as ArrayList<ProductModel>)
+            Log.i(TAG, "onActivityResult retrieve: $shopPlan")
+            adapter.addItem(shopPlan)
+        }
     }
 
     fun openShopPlanForm(view: View) {
         val intent = Intent(this, ShopPlanFormActivity::class.java)
-        startActivity(intent)
+        startActivityForResult(intent, SHOP_PLAN_FORM_REQUEST_CODE)
     }
-
-
 }
+
