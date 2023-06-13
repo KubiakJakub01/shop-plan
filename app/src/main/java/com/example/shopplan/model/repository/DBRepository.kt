7f3ce4423103aa163c47047f6834d31c0c58907a
James Kubiak <kubiak.jakub01@gmail.com>
2023-06-13 17:39:53 +0200

    Make DBRepository as a singleton class

--- app/src/main/java/com/example/shopplan/model/repository/DBRepository.kt
+++ app/src/main/java/com/example/shopplan/model/repository/DBRepository.kt
@@ -1,18 +1,33 @@
 package com.example.shopplan.model.repository
 
+import android.content.Context
 import com.example.shopplan.model.dao.ShopPlanDbHelper
 import com.example.shopplan.model.menager.ProductManager
 import com.example.shopplan.model.menager.ShopPlanManager
 
-class DBRepository (dbHelper: ShopPlanDbHelper) {
+/**
+ * Repository singleton class for managing database access
+ */
+class DBRepository private constructor(context: Context) {
     private var dbHelper: ShopPlanDbHelper
     private var shopPlanManager: ShopPlanManager
     private var productManager: ProductManager
 
     init {
-        this.dbHelper = dbHelper
+        this.dbHelper = ShopPlanDbHelper(context)
         shopPlanManager = ShopPlanManager(dbHelper)
         productManager = ProductManager(dbHelper)
+    }
+
+    companion object {
+        @Volatile
+        private var instance: DBRepository? = null
+
+        fun getInstance(context: Context): DBRepository {
+            return instance ?: synchronized(this) {
+                instance ?: DBRepository(context.applicationContext).also { instance = it }
+            }
+        }
     }
 
     fun getShopPlanManager(): ShopPlanManager {
