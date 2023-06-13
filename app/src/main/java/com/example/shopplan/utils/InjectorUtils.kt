21ba90cdb8936117f5cc4cb3e3679de3dde4a810
James Kubiak <kubiak.jakub01@gmail.com>
2023-06-13 19:22:23 +0200

    Init InjectionUtils

---
+++ app/src/main/java/com/example/shopplan/utils/InjectorUtils.kt
@@ -0,0 +1,14 @@
+package com.example.shopplan.utils
+
+import android.content.Context
+import com.example.shopplan.model.dao.Database
+import com.example.shopplan.model.repository.ShopPlanRepository
+import com.example.shopplan.ui.shopplan.ShopPlanModelViewFactory
+
+object InjectorUtils {
+
+    fun provideShopPlanViewModelFactory(context: Context): ShopPlanModelViewFactory {
+        val shopPlanRepository = ShopPlanRepository.getInstance(Database.getInstance(context).getShopPlanDao())
+        return ShopPlanModelViewFactory(shopPlanRepository)
+    }
+}
