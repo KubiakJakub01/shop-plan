c3a6aabb625d9822dbe7f3bcf8e46d6fa05378d0
James Kubiak <kubiak.jakub01@gmail.com>
2023-06-13 19:22:02 +0200

    Create ShopPlanViewModel class

---
+++ app/src/main/java/com/example/shopplan/ui/shopplan/ShopPlanViewModel.kt
@@ -0,0 +1,15 @@
+package com.example.shopplan.ui.shopplan
+
+import androidx.lifecycle.ViewModel
+import com.example.shopplan.model.repository.ShopPlanRepository
+import com.example.shopplan.model.table.ShopPlanModel
+
+class ShopPlanViewModel(private val shopPlanRepository: ShopPlanRepository)
+    : ViewModel() {
+
+    fun addShopPlan(shopPlan: ShopPlanModel) {
+        shopPlanRepository.addShopPlan(shopPlan)
+    }
+
+    fun getShopPlans() = shopPlanRepository.getShopPlans()
+}
