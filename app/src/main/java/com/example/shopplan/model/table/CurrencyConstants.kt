59c6f16cdab8dce1484cb6aa696093cded0b877e
James Kubiak <kubiak.jakub01@gmail.com>
2023-06-15 18:36:23 +0200

    Add CurrencyConstatns with currency and symbols

---
+++ app/src/main/java/com/example/shopplan/model/table/CurrencyConstants.kt
@@ -0,0 +1,21 @@
+package com.example.shopplan.model.table
+
+class CurrencyConstants {
+    companion object {
+        const val EUR = "EUR"
+        const val USD = "USD"
+        const val GBP = "GBP"
+        const val JPY = "JPY"
+        const val PLN = "PLN"
+
+        val currencyList = listOf(EUR, USD, GBP, JPY, PLN)
+
+        val currencySymbols = mapOf(
+            EUR to "€",
+            USD to "$",
+            GBP to "£",
+            JPY to "¥",
+            PLN to "zł"
+        )
+    }
+}
